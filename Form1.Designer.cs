9c1f5bd0a1d6286378123d0972149e064ea44fc7
SANTIAGO LOPEZ <santiagolopezslz60@gmail.com>
2024-05-05 18:14:52 -0300

    Lista

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,13 +32,15 @@
             btnBarco = new Button();
             btnAvion = new Button();
             btnAuto = new Button();
+            btnListar = new Button();
+            lstVehiculos = new ListBox();
             ((System.ComponentModel.ISupportInitialize)pctImagen).BeginInit();
             SuspendLayout();
             // 
             // pctImagen
             // 
             pctImagen.Image = Properties.Resources.auto;
-            pctImagen.Location = new Point(243, 63);
+            pctImagen.Location = new Point(417, 72);
             pctImagen.Name = "pctImagen";
             pctImagen.Size = new Size(520, 323);
             pctImagen.SizeMode = PictureBoxSizeMode.Zoom;
@@ -47,43 +49,66 @@
             // 
             // btnBarco
             // 
-            btnBarco.Location = new Point(728, 411);
+            btnBarco.Location = new Point(902, 420);
             btnBarco.Name = "btnBarco";
             btnBarco.Size = new Size(94, 29);
             btnBarco.TabIndex = 6;
             btnBarco.Text = "Barco";
             btnBarco.UseVisualStyleBackColor = true;
+            btnBarco.Click += btnBarco_Click_1;
             // 
             // btnAvion
             // 
-            btnAvion.Location = new Point(437, 411);
+            btnAvion.Location = new Point(611, 420);
             btnAvion.Name = "btnAvion";
             btnAvion.Size = new Size(94, 29);
             btnAvion.TabIndex = 5;
             btnAvion.Text = "Avion";
             btnAvion.UseVisualStyleBackColor = true;
+            btnAvion.Click += btnAvion_Click_1;
             // 
             // btnAuto
             // 
-            btnAuto.Location = new Point(197, 411);
+            btnAuto.Location = new Point(371, 420);
             btnAuto.Name = "btnAuto";
             btnAuto.Size = new Size(94, 29);
             btnAuto.TabIndex = 4;
             btnAuto.Text = "Auto";
             btnAuto.UseVisualStyleBackColor = true;
+            btnAuto.Click += btnAuto_Click_1;
+            // 
+            // btnListar
+            // 
+            btnListar.Location = new Point(12, 72);
+            btnListar.Name = "btnListar";
+            btnListar.Size = new Size(94, 29);
+            btnListar.TabIndex = 8;
+            btnListar.Text = "Listar";
+            btnListar.UseVisualStyleBackColor = true;
+            btnListar.Click += btnListar_Click_1;
+            // 
+            // lstVehiculos
+            // 
+            lstVehiculos.FormattingEnabled = true;
+            lstVehiculos.ItemHeight = 20;
+            lstVehiculos.Location = new Point(12, 107);
+            lstVehiculos.Name = "lstVehiculos";
+            lstVehiculos.Size = new Size(150, 104);
+            lstVehiculos.TabIndex = 9;
             // 
             // frmVehiculos
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1018, 502);
+            Controls.Add(lstVehiculos);
+            Controls.Add(btnListar);
             Controls.Add(pctImagen);
             Controls.Add(btnBarco);
             Controls.Add(btnAvion);
             Controls.Add(btnAuto);
             Name = "frmVehiculos";
             Text = "Vehiculos";
-            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pctImagen).EndInit();
             ResumeLayout(false);
         }
@@ -94,5 +119,7 @@
         private Button btnBarco;
         private Button btnAvion;
         private Button btnAuto;
+        private Button btnListar;
+        private ListBox lstVehiculos;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -13,43 +13,69 @@ namespace pryLopezEtapa4
         https://www.c-sharpcorner.com/article/c-sharp-list/ 
         Desde un botón “listar” mostrar todos los objetos por su nombre.*/
 
+        private List<clsVehiculo> vehiculos; // Lista para almacenar los vehículos
+
         private clsVehiculo auto;
         private clsVehiculo barco;
         private clsVehiculo avion;
         public frmVehiculos()
         {
             InitializeComponent();
-            InitializeComponent();
+
             auto = new clsVehiculo("Auto", "Auto", Properties.Resources.auto);
             barco = new clsVehiculo("Barco", "Barco", Properties.Resources.barco);
             avion = new clsVehiculo("Avion", "Avion", Properties.Resources.avion);
+
+            // Inicializa la lista de vehículos
+            vehiculos = new List<clsVehiculo>();
+
+            // No necesitas inicializar los vehículos aquí
         }
+
+        // Método para mostrar un vehículo en el PictureBox
         private void MostrarVehiculo(clsVehiculo vehiculo)
         {
             pctImagen.Image = vehiculo.Imagen;
             pctImagen.SizeMode = PictureBoxSizeMode.StretchImage;
         }
 
-        private void btnAvion_Click(object sender, EventArgs e)
+        // Evento para el botón Listar
+
+        private void btnListar_Click_1(object sender, EventArgs e)
         {
-            MostrarVehiculo(avion);
+            // Mostrar los nombres de todos los vehículos en un ListBox
+            lstVehiculos.Items.Clear();
+            foreach (clsVehiculo vehiculo in vehiculos)
+            {
+                lstVehiculos.Items.Add(vehiculo.Nombre);
+            }
         }
 
-        private void btnBarco_Click(object sender, EventArgs e)
+        private void btnAuto_Click_1(object sender, EventArgs e)
         {
 
-            MostrarVehiculo(barco);
+            // Crea un nuevo objeto clsVehiculo y agrégalo a la lista
+            clsVehiculo auto = new clsVehiculo("Auto", "Auto", Properties.Resources.auto);
+            vehiculos.Add(auto);
+            MostrarVehiculo(auto); // Muestra la imagen del auto
         }
 
-        private void btnAuto_Click(object sender, EventArgs e)
+        private void btnAvion_Click_1(object sender, EventArgs e)
         {
 
-            MostrarVehiculo(auto);
+            // Crea un nuevo objeto clsVehiculo y agrégalo a la lista
+            clsVehiculo avion = new clsVehiculo("Avion", "Avion", Properties.Resources.avion);
+            vehiculos.Add(avion);
+            MostrarVehiculo(avion); // Muestra la imagen del avión
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnBarco_Click_1(object sender, EventArgs e)
         {
 
+            // Crea un nuevo objeto clsVehiculo y agrégalo a la lista
+            clsVehiculo barco = new clsVehiculo("Barco", "Barco", Properties.Resources.barco);
+            vehiculos.Add(barco);
+            MostrarVehiculo(barco); // Muestra la imagen del barco
         }
     }
 }
